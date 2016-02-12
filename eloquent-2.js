51107b08205f99c65aa5bb8caaa41ed9baac5c8a
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-02-13 10:47:13 +1300

    make minor edits to eloquent-2.js

--- eloquent-2.js
+++ eloquent-2.js
@@ -25,43 +25,29 @@ for(var i = 1; i <= 100; i++) {
 
 //Exercise Three:
 
-Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
-
-Passing this string to console.log should show something like this:
-
- # # # #
-# # # #
- # # # #
-# # # #
- # # # #
-# # # #
- # # # #
-# # # #
-When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
-
 var size = Number(prompt("Grid Size: "));
-var s = "";
+var str = "";
 for(var row = 1; row <= size; row++) {
   if(row % 2 !== 0) {
     for(var col = 1; col <= size; col++) {
       if(col % 2 !== 0) {
-        s+= " ";
+        str+= " ";
       }
       else {
-        s+= "#";
+        str+= "#";
       }
     }
   }
   else {
     for(var col = 1; col <= size; col++) {
       if(col % 2 !== 0) {
-        s+= "#";
+        str+= "#";
       }
       else {
-        s+= " ";
+        str+= " ";
       }
     }
   } 
-  s+= "\n"; 
+  str+= "\n"; 
 }
-console.log(s);
+console.log(str);
